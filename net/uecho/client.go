2b434ec1289c22c2b0834d3d44e2f188f5cb7787
Satoshi Konno <skonno@cybergarage.org>
2018-03-18 01:38:18 +0900

    Added initial source files.

---
+++ net/uecho/client.go
@@ -0,0 +1,24 @@
+// Copyright (C) 2018 The uecho-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package uecho
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+)
+
+// Client is an instance for Graphite protocols.
+type Client struct {
+}
+
+// NewClient returns a new Client.
+func NewClient() *Client {
+	client := &Client{
+		}
+	return client
+}
