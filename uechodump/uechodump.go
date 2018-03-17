90870bd4c3cbacf31570da5c52a273f7b6058919
Satoshi Konno <skonno@cybergarage.org>
2018-03-18 02:21:29 +0900

    Added main.

---
+++ uechodump/uechodump.go
@@ -0,0 +1,23 @@
+// Copyright 2014 The uecho-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+uechodump is a dump utility for Echonet Lite.
+
+	NAME
+	uechodump
+
+	SYNOPSIS
+	uechodump [OPTIONS]
+
+	DESCRIPTION
+	uechodump is a dump utility for Echonet Lite.
+
+	RETURN VALUE
+	  Return EXIT_SUCCESS or EXIT_FAILURE
+*/
+package main
+
+func main() {
+}
