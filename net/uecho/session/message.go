0651620bace20bf0b0f4f65a6793dd124e7246e0
Satoshi Konno <skonno@cybergarage.org>
2018-04-24 02:22:07 +0900

    Updated message.

--- net/uecho/session/message.go
+++ net/uecho/session/message.go
@@ -9,21 +9,41 @@ import (
 )
 
 const (
-	uEchoMessageHeaderLen = (1 + 1 + 2)
-	uEchoMessageMinLen    = (uEchoMessageHeaderLen + 3 + 3 + 1 + 1)
-	uEchoEhd1             = 0x10
-	uEchoEhd2             = 0x81
-	uEchoTIDSize          = 2
-	uEchoEOJSize          = 3
+	MessageHeaderLen = (1 + 1 + 2)
+	MessageMinLen    = (MessageHeaderLen + 3 + 3 + 1 + 1)
+	EHD1             = 0x10
+	EHD2             = 0x81
+	TIDSize          = 2
+	TIDMax           = 65535
+	EOJSize          = 3
+)
+
+const (
+	EsvWriteRequest                      = 0x60
+	EsvWriteRequestResponseRequired      = 0x61
+	EsvReadRequest                       = 0x62
+	EsvNotificationRequest               = 0x63
+	EsvWriteReadRequest                  = 0x6E
+	EsvWriteResponse                     = 0x71
+	EsvReadResponse                      = 0x72
+	EsvNotification                      = 0x73
+	EsvNotificationResponseRequired      = 0x74
+	EsvNotificationResponse              = 0x7A
+	EsvWriteReadResponse                 = 0x7E
+	EsvWriteRequestError                 = 0x50
+	EsvWriteRequestResponseRequiredError = 0x51
+	EsvReadRequestError                  = 0x52
+	EsvNotificationRequestError          = 0x53
+	EsvWriteReadRequestError             = 0x5E
 )
 
 // Message is an instance for Echonet message.
 type Message struct {
 	EHD1      byte
 	EHD2      byte
-	TID       [uEchoTIDSize]byte
-	SEOJ      [uEchoEOJSize]byte
-	DEOJ      [uEchoEOJSize]byte
+	TID       [TIDSize]byte
+	SEOJ      [EOJSize]byte
+	DEOJ      [EOJSize]byte
 	ESV       int
 	OPC       byte
 	Property  *Property
@@ -35,7 +55,10 @@ type Message struct {
 
 // NewMessage returns a new message.
 func NewMessage() *Message {
-	msg := &Message{}
+	msg := &Message{
+		EHD1: EHD1,
+		EHD2: EHD2,
+	}
 	return msg
 }
 
@@ -47,6 +70,22 @@ func NewMessageWithBytes(data []byte) (*Message, error) {
 		return nil, err
 	}
 	return msg, nil
+}
+
+// SetID sets the specified ID.
+func (msg *Message) SetID(value int) error {
+	if TIDMax < value {
+		value %= TIDMax
+	}
+	msg.TID[0] = (byte)((value & 0xFF00) >> 8)
+	msg.TID[1] = (byte)(value & 0x00FF)
+
+	return nil
+}
+
+// GetID returns the stored ID.
+func (msg *Message) GetID(value int) int {
+	return (((int)(msg.TID[0]) << 8) + (int)(msg.TID[1]))
 }
 
 // Parse parses the specified bytes.
