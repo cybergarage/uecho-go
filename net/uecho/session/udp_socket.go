f779d040b31170c1cf5a544a9264818ad42aabb4
Satoshi Konno <skonno@cybergarage.org>
2018-04-23 17:19:17 +0900

    Added networking pakages for UDP requests.

---
+++ net/uecho/session/udp_socket.go
@@ -0,0 +1,66 @@
+// Copyright 2017 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package session
+
+import (
+	"errors"
+	"net"
+)
+
+const (
+	errorSocketIsClosed = "Socket is closed"
+)
+
+// A UDPSocket represents a socket for UDP.
+type UDPSocket struct {
+	Conn      *net.UDPConn
+	readBuf   []byte
+	Interface net.Interface
+}
+
+// NewUDPSocket returns a new UDPSocket.
+func NewUDPSocket() *UDPSocket {
+	sock := &UDPSocket{}
+	sock.readBuf = make([]byte, MAX_PACKET_SIZE)
+	return sock
+}
+
+// Close closes the current opened socket.
+func (sock *UDPSocket) Close() error {
+	if sock.Conn == nil {
+		return nil
+	}
+	err := sock.Conn.Close()
+	if err != nil {
+		return err
+	}
+
+	sock.Conn = nil
+	sock.Interface = net.Interface{}
+
+	return nil
+}
+
+// Read reads from the current opened socket.
+func (sock *UDPSocket) Read() (*Message, error) {
+	if sock.Conn == nil {
+		return nil, errors.New(errorSocketIsClosed)
+	}
+
+	n, from, err := sock.Conn.ReadFromUDP(sock.readBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	msg, err := NewMessageWithBytes(sock.readBuf[:n])
+	if err != nil {
+		return nil, err
+	}
+
+	msg.From = *from
+	msg.Interface = sock.Interface
+
+	return msg, nil
+}
