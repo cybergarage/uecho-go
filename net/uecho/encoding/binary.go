369aec14471d7ed1ef0cdfc1a954e8a49145e05a
Satoshi Konno <skonno@cybergarage.org>
2018-05-21 19:14:57 +0900

    Add functions for binary numbers.

---
+++ net/uecho/encoding/binary.go
@@ -0,0 +1,25 @@
+// Copyright (C) 2018 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package encoding
+
+// IntegerToByte converts a specified integer to bytes
+func IntegerToByte(v uint, b []byte) {
+	byteSize := len(b)
+	for n := 0; n < byteSize; n++ {
+		idx := ((byteSize - 1) - n)
+		b[idx] = byte((v >> (uint(n) * 8)) & 0xFF)
+	}
+}
+
+// ByteToInteger converts specified bytes to a integer
+func ByteToInteger(b []byte) uint {
+	var v uint
+	byteSize := len(b)
+	for n := 0; n < byteSize; n++ {
+		idx := ((byteSize - 1) - n)
+		v += uint((uint(b[idx]) << (uint(n) * 8)))
+	}
+	return v
+}
---
+++ net/uecho/encoding/binary_test.go
@@ -0,0 +1,51 @@
+// Copyright (C) 2018 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package encoding
+
+import (
+	"testing"
+)
+
+func TestBinaryEncoding(t *testing.T) {
+	var n uint
+
+	intBytes := make([]byte, 1)
+	for n = 0; n <= 0xFF; n++ {
+		IntegerToByte(n, intBytes)
+		if n != ByteToInteger(intBytes) {
+			t.Errorf("[1:%d] : %d != %d", n, ByteToInteger(intBytes), n)
+		}
+	}
+
+	intBytes = make([]byte, 2)
+	for n = 0; n <= 0xFFFF; n += (0xFFFF / 0xFF) {
+		IntegerToByte(n, intBytes)
+		if n != ByteToInteger(intBytes) {
+			t.Errorf("[2:%d] : %d != %d", n, ByteToInteger(intBytes), n)
+		}
+	}
+
+	intBytes = make([]byte, 3)
+	for n = 0; n <= 0xFFFFFF; n += (0xFFFFFF / 0xFF) {
+		IntegerToByte(n, intBytes)
+		if n != ByteToInteger(intBytes) {
+			t.Errorf("[3:%d] : %d != %d", n, ByteToInteger(intBytes), n)
+		}
+	}
+
+	intBytes = make([]byte, 4)
+	for n = 0; n < 0xFFFFFFFF; n += (0xFFFFFFFF / 0xFF) {
+		IntegerToByte(n, intBytes)
+		if n != ByteToInteger(intBytes) {
+			t.Errorf("[4:%d] : %d != %d", n, ByteToInteger(intBytes), n)
+		}
+	}
+}
+
+func TestBinaryEncoding(t *testing.T) {
+	BOOST_CHECK_EQUAL(uecho_objectcode2classcode(0x0EF001), 0x0EF0)
+	BOOST_CHECK_EQUAL(uecho_objectcode2classcode(0x001102), 0x0011)
+	BOOST_CHECK_EQUAL(uecho_objectcode2classcode(0x001102), 0x0011)
+}
