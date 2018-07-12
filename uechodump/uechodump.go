a7ef42c8dc317c1c88be0266531ab3bb8b7a351f
Satoshi Konno <skonno@cybergarage.org>
2018-07-12 14:45:51 +0900

    Update uechodump

--- uechodump/uechodump.go
+++ uechodump/uechodump.go
@@ -1,4 +1,4 @@
-// Copyright 2014 The uecho-go Authors. All rights reserved.
+// Copyright (C) 2018 The uecho-go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
