665ceff3abbf455111de0b77c63fa08aeaf32916
Satoshi Konno <skonno@cybergarage.org>
2018-07-12 14:45:51 +0900

    Update uechodump

--- uechodump/uechodump.go
+++ uechodump/uechodump.go
@@ -1,4 +1,4 @@
-// Copyright 2014 Yahoo Japan Corporation. All rights reserved.
+// Copyright (C) 2018 Satoshi Konno. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
