604d2e2d623719bd9d2c4450d0bf8c2fd18047f0
Satoshi Konno <skonno@cybergarage.org>
2018-09-09 00:50:18 +0900

    Rename ObjectListener::MessageRequestReceived() to RequestMessageReceived()

--- net/echonet/controller_test.go
+++ net/echonet/controller_test.go
@@ -29,7 +29,7 @@ func newTestController() *testController {
 	return ctrl
 }
 
-func (ctrl *testController) MessageRequestReceived(*protocol.Message) {
+func (ctrl *testController) RequestMessageReceived(*protocol.Message) {
 
 }
 
