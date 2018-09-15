95c713f2bec895a9f202a89c33b1c1f2b4a0e223
Satoshi Konno <skonno@cybergarage.org>
2018-09-15 15:11:58 +0900

    Update Message to parse io.Reader for TCP connection

---
+++ net/echonet/protocol/message_parser_reader.go
@@ -0,0 +1,86 @@
+// Copyright (C) 2018 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package protocol
+
+import (
+	"fmt"
+	"io"
+)
+
+// parseFormat1PropertyReader parses the specified property reader.
+func (msg *Message) parseFormat1PropertyReader(reader io.Reader) error {
+	propertyHeader := make([]byte, Format1PropertyHeaderSize)
+
+	for n := 0; n < int(msg.OPC); n++ {
+		prop := msg.GetProperty(n)
+		if prop == nil {
+			continue
+		}
+
+		nRead, err := reader.Read(propertyHeader)
+		if err != nil {
+			return err
+		}
+		if nRead < Format1PropertyHeaderSize {
+			return fmt.Errorf(errorShortMessageSize, n, Format1PropertyHeaderSize)
+		}
+
+		prop.Code = PropertyCode(propertyHeader[0])
+
+		propDataSize := int(propertyHeader[1])
+		prop.Data = make([]byte, propDataSize)
+		nRead, err = reader.Read(prop.Data)
+		if err != nil {
+			return err
+		}
+		if nRead < propDataSize {
+			return fmt.Errorf(errorShortMessageSize, n, propDataSize)
+		}
+	}
+
+	return nil
+}
+
+// ParseReader parses the specified bytes.
+func (msg *Message) ParseReader(reader io.Reader) error {
+	// Frame header
+
+	frameHeader := make([]byte, FrameHeaderSize)
+	n, err := reader.Read(frameHeader)
+	if err != nil {
+		return err
+	}
+	if n < FrameHeaderSize {
+		return fmt.Errorf(errorShortMessageSize, n, FrameHeaderSize)
+	}
+	err = msg.parseFrameHeaderBytes(frameHeader)
+	if err != nil {
+		return err
+	}
+
+	// Echonet Format1 Header
+
+	format1Header := make([]byte, Format1HeaderSize)
+	n, err = reader.Read(format1Header)
+	if err != nil {
+		return err
+	}
+	if n < Format1HeaderSize {
+		return fmt.Errorf(errorShortMessageSize, n, Format1HeaderSize)
+	}
+	err = msg.parseFormat1HeaderBytes(format1Header)
+	if err != nil {
+		return err
+	}
+
+	// Propety data
+
+	err = msg.parseFormat1PropertyReader(reader)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
