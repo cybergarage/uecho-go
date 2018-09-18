baaae40b4a0361f821b79a73b07d831051ef0592
Satoshi Konno <skonno@cybergarage.org>
2018-09-18 13:22:30 +0900

    Fix testing failures to set the lastest message more correctly

--- net/echonet/protocol/message_test.go
+++ net/echonet/protocol/message_test.go
@@ -7,6 +7,8 @@ package protocol
 import (
 	"bytes"
 	"testing"
+
+	"github.com/cybergarage/uecho-go/net/echonet/encoding"
 )
 
 func TestNewMessage(t *testing.T) {
@@ -14,6 +16,64 @@ func TestNewMessage(t *testing.T) {
 	_, err := NewMessageWithMessage(msg)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestNewFormat1TestMessage(t *testing.T) {
+	tid := 100
+	tidBytes := make([]byte, 2)
+	encoding.IntegerToByte(uint(tid), tidBytes)
+
+	opc := 3
+
+	testMessageBytes := []byte{
+		EHD1Echonet,
+		EHD2Format1,
+		tidBytes[0], tidBytes[1],
+		0xA0, 0xB0, 0xC0,
+		0xD0, 0xE0, 0xF0,
+		ESVNotification,
+		byte(opc),
+		1, 1, 'a',
+		2, 2, 'b', 'c',
+		3, 3, 'c', 'd', 'e',
+	}
+
+	msg, err := NewMessageWithBytes(testMessageBytes)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !msg.IsTID(uint(tid)) {
+		t.Errorf("%d != %d", msg.GetTID(), tid)
+	}
+
+	if !msg.IsSourceObjectCode(0xA0B0C0) {
+		t.Errorf("%X != %X", msg.GetSourceObjectCode(), 0xA0B0C0)
+	}
+
+	if !msg.IsDestinationObjectCode(0xD0E0F0) {
+		t.Errorf("%X != %X", msg.GetDestinationObjectCode(), 0xD0E0F0)
+	}
+
+	if !msg.IsESV(ESVNotification) {
+		t.Errorf("%X != %X", msg.GetESV(), ESVNotification)
+	}
+
+	if msg.GetOPC() != opc {
+		t.Errorf("%d != %d", msg.GetOPC(), opc)
+	}
+
+	for n := 1; n <= opc; n++ {
+		prop := msg.GetProperty(n - 1)
+		if prop.Code != PropertyCode(n) {
+			t.Errorf("%d != %d", prop.Code, n)
+		}
+		if prop.Size() != n {
+			t.Errorf("%d != %d", prop.Size(), n)
+		}
+
 	}
 }
 
