fd002b05d14bc9384996be21429fcf9fe6909253
Satoshi Konno <skonno@cybergarage.org>
2018-09-18 18:01:25 +0900

    Add Config to enable or disable the transport functions

---
+++ net/echonet/config_test.go
@@ -0,0 +1,13 @@
+// Copyright 2018 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package echonet
+
+import (
+	"testing"
+)
+
+func TestNewDefaultConfig(t *testing.T) {
+	NewDefaultConfig()
+}
