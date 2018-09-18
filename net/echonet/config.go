75e407c67b315a366828037555954f696046d999
Satoshi Konno <skonno@cybergarage.org>
2018-09-18 18:01:25 +0900

    Add Config to enable or disable the transport functions

---
+++ net/echonet/config.go
@@ -0,0 +1,24 @@
+// Copyright 2018 The uecho-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package echonet
+
+import (
+	"github.com/cybergarage/uecho-go/net/echonet/transport"
+)
+
+type transportConfig = transport.Config
+
+// Config represents a cofiguration for transport.
+type Config struct {
+	*transportConfig
+}
+
+// NewDefaultConfig returns a default configuration.
+func NewDefaultConfig() *Config {
+	conf := &Config{
+		transportConfig: transport.NewDefaultConfig(),
+	}
+	return conf
+}
