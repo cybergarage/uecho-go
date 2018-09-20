ee6d236120f80ba175225bf6031e0da4d98f0bd5
Satoshi Konno <skonno@cybergarage.org>
2018-09-20 13:30:57 +0900

    Add utility functions to logger

---
+++ net/echonet/log/logger_func.go
@@ -0,0 +1,32 @@
+// Copyright (C) 2018 The uecho-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package log
+
+// SetDebugEnabled sets the logging level of the current shared logger to trace.
+func SetDebugEnabled(flag bool) {
+	logger := GetSharedLogger()
+	if logger == nil {
+		return
+	}
+	logger.SetLevel(LoggerLevelTrace)
+}
+
+// SetStdoutEnbled sets the shared logger to stdout
+func SetStdoutEnbled(flag bool) {
+	if flag {
+		logger := GetSharedLogger()
+		if logger == nil {
+			return
+		}
+		SetSharedLogger(NewStdoutLogger(logger.GetLevel()))
+	}
+}
+
+// SetStdoutDebugEnbled sets a trace stdout logger for debug.
+func SetStdoutDebugEnbled(flag bool) {
+	if flag {
+		SetSharedLogger(NewStdoutLogger(LoggerLevelTrace))
+	}
+}
