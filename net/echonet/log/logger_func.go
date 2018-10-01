c9456d7ad5274b8dee894b7bbc2753b1a018422d
Satoshi Konno <skonno@cybergarage.org>
2018-10-01 20:17:13 +0900

    Remove deprecated SetDebugEnabled() and SetStdoutEnbled()

--- net/echonet/log/logger_func.go
+++ net/echonet/log/logger_func.go
@@ -4,26 +4,6 @@
 
 package log
 
-// SetDebugEnabled sets the logging level of the current shared logger to trace.
-func SetDebugEnabled(flag bool) {
-	logger := GetSharedLogger()
-	if logger == nil {
-		return
-	}
-	logger.SetLevel(LoggerLevelTrace)
-}
-
-// SetStdoutEnbled sets the shared logger to stdout
-func SetStdoutEnbled(flag bool) {
-	if flag {
-		logger := GetSharedLogger()
-		if logger == nil {
-			return
-		}
-		SetSharedLogger(NewStdoutLogger(logger.GetLevel()))
-	}
-}
-
 // SetStdoutDebugEnbled sets a trace stdout logger for debug.
 func SetStdoutDebugEnbled(flag bool) {
 	if flag {
