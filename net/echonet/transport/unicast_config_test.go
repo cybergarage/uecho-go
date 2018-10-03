d1553ea2440a175d2e15d8229b217bcfe2661b98
Satoshi Konno <skonno@cybergarage.org>
2018-10-03 10:45:46 +0900

    Add UnicastConfig::Equals() for testing

--- net/echonet/transport/unicast_config_test.go
+++ net/echonet/transport/unicast_config_test.go
@@ -6,8 +6,48 @@ package transport
 
 import (
 	"testing"
+	"time"
 )
 
 func TestNewUnicastDefaultConfig(t *testing.T) {
 	NewDefaultUnicastConfig()
+}
+
+func TestUnicastEquals(t *testing.T) {
+	conf01 := NewDefaultUnicastConfig()
+	conf02 := NewDefaultUnicastConfig()
+
+	// Testing Set*()
+
+	if !conf01.Equals(conf02) {
+		t.Errorf("%v != %v", conf01, conf02)
+	}
+
+	conf01.SetTCPEnabled(true)
+	conf02.SetTCPEnabled(false)
+	if conf01.Equals(conf02) {
+		t.Errorf("%v == %v", conf01, conf02)
+	}
+
+	conf01.SetConnectionTimeout(time.Microsecond)
+	conf02.SetConnectionTimeout(time.Second)
+	if conf01.Equals(conf02) {
+		t.Errorf("%v == %v", conf01, conf02)
+	}
+
+	// Testing SetConfig()
+
+	conf03 := NewDefaultUnicastConfig()
+	conf03.SetConfig(conf01)
+	if !conf01.Equals(conf03) {
+		t.Errorf("%v != %v", conf01, conf03)
+	}
+	if conf02.Equals(conf03) {
+		t.Errorf("%v == %v", conf01, conf02)
+	}
+
+	conf03.SetConfig(conf02)
+	if !conf02.Equals(conf03) {
+		t.Errorf("%v != %v", conf01, conf03)
+	}
 }
