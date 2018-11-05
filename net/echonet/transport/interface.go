1ceb2f2bccca1411724bba67282e056dbe0d11ad
Satoshi Konno <skonno@cybergarage.org>
2018-11-05 19:35:07 +0900

    Fix transport.IsIPv6Address() for IPv6 local address

--- net/echonet/transport/interface.go
+++ net/echonet/transport/interface.go
@@ -17,7 +17,7 @@ const (
 
 // IsIPv6Address retusn true whether the specified address is a IPv6 address
 func IsIPv6Address(addr string) bool {
-	if 0 < strings.Index(addr, ":") {
+	if 0 <= strings.Index(addr, ":") {
 		return true
 	}
 
