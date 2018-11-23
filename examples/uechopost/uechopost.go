fabf323bcae9d8391fb0381c34d8f1bdae3b8fed
Satoshi Konno <skonno@cybergarage.org>
2018-11-23 22:36:47 +0900

    Fix error

--- examples/uechopost/uechopost.go
+++ examples/uechopost/uechopost.go
@@ -51,8 +51,8 @@ func outputTransportMessage(prefix string, addr string, obj echonet.ObjectCode, 
 
 func outputRequestMessage(ctrl *PostController, msg *echonet.Message) {
 	sourceAddr := ""
-	boundAddrs := ctrl.GetBoundAddresses()
-	if 0 < len(boundAddrs) {
+	boundAddrs, err := ctrl.GetBoundAddresses()
+	if (err != nil) && (0 < len(boundAddrs)) {
 		sourceAddr = boundAddrs[0]
 	}
 	outputTransportMessage("->", sourceAddr, msg.GetDestinationObjectCode(), msg)
