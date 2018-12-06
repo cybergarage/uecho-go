96f605892ed006b416aae3db380368aac392d0e1
Satoshi Konno <skonno@cybergarage.org>
2018-12-06 23:39:28 +0900

    Add MulticastSocket::Listen() to implement using 'golang.org/x/net/ipv4' for CentOS #2

---
+++ net/echonet/transport/multicast_socket_listen_xnet.go
@@ -0,0 +1,42 @@
+// Copyright 2018 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build linux
+
+package transport
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+
+	"golang.org/x/net/ipv4"
+)
+
+// Listen listens the Ethonet multicast address with the specified interface.
+func (sock *MulticastSocket) Listen(ifi *net.Interface) error {
+	addr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(MulticastAddress, strconv.Itoa(UDPPort)))
+	if err != nil {
+		return err
+	}
+
+	sock.Conn, err = net.ListenUDP("udp4", addr)
+	if err != nil {
+		return fmt.Errorf("%s (%s)", err.Error(), ifi.Name)
+	}
+
+	pktConn := ipv4.NewPacketConn(sock.Conn)
+
+	err = pktConn.SetMulticastLoopback(true)
+	if err != nil {
+		return err
+	}
+
+	err = pktConn.JoinGroup(ifi, addr)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
