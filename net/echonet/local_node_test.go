c1bec33c0df22693efc27e185fe045e0694541eb
Satoshi Konno <skonno@cybergarage.org>
2022-07-17 03:40:29 +0900

    Update testNodeRequestSleep

--- net/echonet/local_node_test.go
+++ net/echonet/local_node_test.go
@@ -16,7 +16,7 @@ import (
 const (
 	testNodeRequestCount   = 4
 	testNodeRequestTimeout = time.Millisecond * 1000
-	testNodeRequestSleep   = time.Millisecond * 100
+	testNodeRequestSleep   = time.Millisecond * 500
 )
 
 const (
