a28036bb57439945a6d65343b1b646b44195f213
Satoshi Konno <skonno@cybergarage.org>
2022-08-05 12:52:05 +0900

    Fix misspell warnings

--- net/echonet/transport/extention_config.go
+++ net/echonet/transport/extention_config.go
@@ -6,16 +6,16 @@ package transport
 
 import "reflect"
 
-// ExtentionConfig represents a cofiguration for extended specifications.
-type ExtentionConfig struct {
+// ExtensionConfig represents a cofiguration for extended specifications.
+type ExtensionConfig struct {
 	AutoPortBindingEnabled      bool
 	EachInterfaceBindingEnabled bool
 	AutoInterfaceBindingEnabled bool
 }
 
-// NewDefaultExtentionConfig returns a default configuration.
-func NewDefaultExtentionConfig() *ExtentionConfig {
-	conf := &ExtentionConfig{
+// NewDefaultExtensionConfig returns a default configuration.
+func NewDefaultExtensionConfig() *ExtensionConfig {
+	conf := &ExtensionConfig{
 		AutoPortBindingEnabled:      false,
 		EachInterfaceBindingEnabled: true,
 		AutoInterfaceBindingEnabled: true,
@@ -24,43 +24,43 @@ func NewDefaultExtentionConfig() *ExtentionConfig {
 }
 
 // SetConfig sets all flags.
-func (conf *ExtentionConfig) SetConfig(newConfig *ExtentionConfig) {
+func (conf *ExtensionConfig) SetConfig(newConfig *ExtensionConfig) {
 	conf.AutoPortBindingEnabled = newConfig.AutoPortBindingEnabled
 	conf.EachInterfaceBindingEnabled = newConfig.EachInterfaceBindingEnabled
 	conf.AutoInterfaceBindingEnabled = newConfig.AutoInterfaceBindingEnabled
 }
 
 // SetAutoPortBindingEnabled sets a flag for TCP functions.
-func (conf *ExtentionConfig) SetAutoPortBindingEnabled(flag bool) {
+func (conf *ExtensionConfig) SetAutoPortBindingEnabled(flag bool) {
 	conf.AutoPortBindingEnabled = flag
 }
 
 // IsAutoPortBindingEnabled returns true whether the TCP function is enabled, otherwise false.
-func (conf *ExtentionConfig) IsAutoPortBindingEnabled() bool {
+func (conf *ExtensionConfig) IsAutoPortBindingEnabled() bool {
 	return conf.AutoPortBindingEnabled
 }
 
 // SetEachInterfaceBindingEnabled sets a flag for binding functions.
-func (conf *ExtentionConfig) SetEachInterfaceBindingEnabled(flag bool) {
+func (conf *ExtensionConfig) SetEachInterfaceBindingEnabled(flag bool) {
 	conf.EachInterfaceBindingEnabled = flag
 }
 
 // IsEachInterfaceBindingEnabled returns true whether the binding functions is enabled, otherwise false.
-func (conf *ExtentionConfig) IsEachInterfaceBindingEnabled() bool {
+func (conf *ExtensionConfig) IsEachInterfaceBindingEnabled() bool {
 	return conf.EachInterfaceBindingEnabled
 }
 
 // SetAutoInterfaceBindingEnabled sets a flag for the auto interface binding.
-func (conf *ExtentionConfig) SetAutoInterfaceBindingEnabled(flag bool) {
+func (conf *ExtensionConfig) SetAutoInterfaceBindingEnabled(flag bool) {
 	conf.AutoInterfaceBindingEnabled = flag
 }
 
 // IsAutoInterfaceBindingEnabled returns true whether the the auto interface binding is enabled, otherwise false.
-func (conf *ExtentionConfig) IsAutoInterfaceBindingEnabled() bool {
+func (conf *ExtensionConfig) IsAutoInterfaceBindingEnabled() bool {
 	return conf.AutoInterfaceBindingEnabled
 }
 
 // Equals returns true whether the specified other class is same, otherwise false.
-func (conf *ExtentionConfig) Equals(otherConf *ExtentionConfig) bool {
+func (conf *ExtensionConfig) Equals(otherConf *ExtensionConfig) bool {
 	return reflect.DeepEqual(conf, otherConf)
 }
