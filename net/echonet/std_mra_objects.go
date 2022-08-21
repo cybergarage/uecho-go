b6aca15d962b60dd983949b55cc7c608493cc084
Satoshi Konno <skonno@cybergarage.org>
2022-08-21 23:46:09 +0900

    Update StandardDatabase::initObjects()

--- net/echonet/std_mra_objects.go
+++ net/echonet/std_mra_objects.go
@@ -34,6 +34,7 @@ func newStandardProperty(code PropertyCode, name string, dataType string, dataSi
 // nolint:misspell, whitespace, maintidx
 func (db *StandardDatabase) initObjects() {
 	var obj *Object
+
 	// Super class (0x0000)
 	obj = newStandardObject("Super class", 0x00, 0x00)
 	obj.AddProperty(newStandardProperty(0x80, "Operation status", "", 0, "required", "optional", "required"))
