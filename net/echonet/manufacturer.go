f5a3e4ae38c1bdcd2805c6332c5b04f814570e1b
Satoshi Konno <skonno@cybergarage.org>
2022-08-21 20:25:48 +0900

    Add NewManufacture()

---
+++ net/echonet/manufacturer.go
@@ -0,0 +1,22 @@
+// Copyright (C) 2018 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package echonet
+
+// ManufactureCode represent a manufacture code.
+type ManufactureCode int
+
+// Manufacture represents a manufacture object.
+type Manufacture struct {
+	Code ManufactureCode
+	Name string
+}
+
+// NewManufacture returns a manufacture instance.
+func NewManufacture(code ManufactureCode, name string) *Manufacture {
+	return &Manufacture{
+		Code: code,
+		Name: name,
+	}
+}
