d78ded4dd6a566efa9461fcd37a0687dae34dd00
Satoshi Konno <skonno@cybergarage.org>
2022-08-21 23:04:24 +0900

    Add GetStandardDatabase()

---
+++ net/echonet/init.go
@@ -0,0 +1,15 @@
+// Copyright (C) 2018 The uecho-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package echonet
+
+var sharedStandardDatabase *StandardDatabase
+
+func init() {
+	sharedStandardDatabase = NewStandardDatabase()
+}
+
+func GetStandardDatabase() *StandardDatabase {
+	return sharedStandardDatabase
+}
