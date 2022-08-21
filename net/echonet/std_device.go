bc456de5a22d47ed08d36d72e6888c5a1b2616f4
Satoshi Konno <skonno@cybergarage.org>
2022-08-22 01:46:58 +0900

    Add NewStandardDeviceWithCodes()

---
+++ net/echonet/std_device.go
@@ -0,0 +1,20 @@
+// Copyright (C) 2018 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package echonet
+
+import (
+	"fmt"
+)
+
+// NewStandardDeviceWithCodes returns a new device of the specified object codes.
+func NewStandardDeviceWithCodes(codes []byte) (*Device, error) {
+	if len(codes) != ObjectCodeSize {
+		return nil, fmt.Errorf(errorInvalidObjectCodes, string(codes))
+	}
+	obj := NewDevice()
+	obj.SetCodes(codes)
+	addStandardProperties(obj.Object)
+	return obj, nil
+}
