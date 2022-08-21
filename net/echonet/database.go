1927f01be696d86f9db693cf480e630935abdcb5
Satoshi Konno <skonno@cybergarage.org>
2022-08-21 20:40:15 +0900

    Add StandardDatabase::GetObject()

--- net/echonet/database.go
+++ net/echonet/database.go
@@ -6,10 +6,35 @@ package echonet
 
 // StandardDatabase represents a standard database of Echonet.
 type StandardDatabase struct {
+	Manufactures map[ManufactureCode]*Manufacture
+	Objects      map[ObjectCode]*Object
 }
 
 // NewStandardDatabase returns a standard database instance.
 func NewStandardDatabase() *StandardDatabase {
-	db := &StandardDatabase{}
+	db := &StandardDatabase{
+		Manufactures: map[ManufactureCode]*Manufacture{},
+		Objects:      map[ObjectCode]*Object{},
+	}
 	return db
+}
+
+func (db *StandardDatabase) addManufacture(man *Manufacture) {
+	db.Manufactures[man.Code] = man
+}
+
+// GetManufacture returns the registered manuracture by the specified manuracture code.
+func (db *StandardDatabase) GetManufacture(code ManufactureCode) (*Manufacture, bool) {
+	m, ok := db.Manufactures[code]
+	return m, ok
+}
+
+func (db *StandardDatabase) addObject(obj *Object) {
+	db.Objects[obj.GetCode()] = obj
+}
+
+// GetObject returns the registered object by the specified object code.
+func (db *StandardDatabase) GetMaGetObjectnufacture(code ObjectCode) (*Object, bool) {
+	obj, ok := db.Objects[code]
+	return obj, ok
 }
