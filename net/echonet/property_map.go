011e62c28a873c395eef3dd7ec8bbf2dc2bcf83e
Satoshi Konno <skonno@cybergarage.org>
2022-08-22 20:19:24 +0900

    Fix PropertyMap::AddProperty()

--- net/echonet/property_map.go
+++ net/echonet/property_map.go
@@ -49,7 +49,6 @@ func (propMap *PropertyMap) GetParentObject() *Object {
 // AddProperty adds a new property into the property map.
 func (propMap *PropertyMap) AddProperty(prop *Property) {
 	propMap.properties[prop.Code()] = prop
-	prop.SetParentObject(propMap.parentObject)
 }
 
 // CreateProperty creates a new property to the property map.
