d374465d1f4174674d43f2450ad851a0b8b1212f
Satoshi Konno <skonno@cybergarage.org>
2022-08-23 02:10:57 +0900

    Add StandardDatabase::SuperObject() and NodeProfile()

---
+++ net/echonet/std_test.go
@@ -0,0 +1,27 @@
+// Copyright (C) 2018 The uecho-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package echonet
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStandardDatabase(t *testing.T) {
+	objCodes := []ObjectCode{
+		0x000000,
+		0x0EF000,
+	}
+
+	db := GetStandardDatabase()
+	for _, objCode := range objCodes {
+		t.Run(fmt.Sprintf("%06X", objCode), func(t *testing.T) {
+			_, ok := db.FindObjectByCode(objCode)
+			if !ok {
+				t.Errorf("%06X object is not found", objCode)
+			}
+		})
+	}
+}
