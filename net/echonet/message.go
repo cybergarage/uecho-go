d94e57033d304f4c5487bdd83afa5ed6e8d74e02
Satoshi Konno <skonno@cybergarage.org>
2022-08-23 00:04:23 +0900

    Add Message::AddProperty() and AddProperties()

--- net/echonet/message.go
+++ net/echonet/message.go
@@ -12,11 +12,20 @@ const (
 	errorNodeRequestInvalidDestinationNode = "Invalid Destination Node : %v"
 )
 
-type Message = protocol.Message
+type Message struct {
+	*protocol.Message
+}
+
+// newMessageWithProtocolMessage returns a new message.
+func newMessageWithProtocolMessage(protoMsg *protocol.Message) *Message {
+	return &Message{
+		Message: protoMsg,
+	}
+}
 
 // NewMessage returns a new message.
 func NewMessage() *Message {
-	return protocol.NewMessage()
+	return newMessageWithProtocolMessage(protocol.NewMessage())
 }
 
 // NewMessageWithParameters returns a new message of the specified parameters.
@@ -25,7 +34,24 @@ func NewMessageWithParameters(objCode ObjectCode, esv ESV, props []*Property) *M
 	msg.SetESV(esv)
 	msg.SetDEOJ(objCode)
 	for _, prop := range props {
-		msg.AddProperty(prop.toProtocolProperty())
+		msg.AddProperty(prop)
 	}
 	return msg
+}
+
+// AddProperty adds a property to the message.
+func (msg *Message) AddProperty(prop *Property) {
+	msg.Message.AddProperty(prop.toProtocolProperty())
+}
+
+// AddProperties adds all properties to the message.
+func (msg *Message) AddProperties(props []*Property) {
+	for _, prop := range props {
+		msg.Message.AddProperty(prop.toProtocolProperty())
+	}
+}
+
+// protocolMessage returns the protocol message.
+func (msg *Message) protocolMessage() *protocol.Message {
+	return msg.Message
 }
