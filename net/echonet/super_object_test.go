c98f3fcfb38a24959914290e380328aebd34c1cd
Satoshi Konno <skonno@cybergarage.org>
2022-08-25 01:03:29 +0900

    Update Test*() to check property maps

--- net/echonet/super_object_test.go
+++ net/echonet/super_object_test.go
@@ -9,20 +9,46 @@ import (
 	"testing"
 )
 
+const (
+	errorMandatoryPropertyNotFound     = "mandatory property (%0X) not found"
+	errorMandatoryPropertyDataNotFound = "mandatory property data (%0X) not found"
+)
+
 func TestSuperObject(t *testing.T) {
 	obj := NewSuperObject()
 
 	mandatoryPropertyCodes := []PropertyCode{
 		ProfileManufacturerCode,
-		ProfileGetPropertyMap,
-		ProfileSetPropertyMap,
-		ProfileAnnoPropertyMap,
 	}
 
 	for _, propCode := range mandatoryPropertyCodes {
 		t.Run(fmt.Sprintf("%02X", propCode), func(t *testing.T) {
 			if !obj.HasProperty(propCode) {
 				t.Errorf(errorMandatoryPropertyNotFound, propCode)
+			}
+		})
+	}
+
+	testObjectPropertyMaps(t, obj.Object)
+}
+
+func testObjectPropertyMaps(t *testing.T, obj *Object) {
+	t.Helper()
+
+	propMapCodes := []PropertyCode{
+		ProfileGetPropertyMap,
+		ProfileSetPropertyMap,
+		ProfileAnnoPropertyMap,
+	}
+
+	for _, propCode := range propMapCodes {
+		t.Run(fmt.Sprintf("%02X", propCode), func(t *testing.T) {
+			prop, ok := obj.FindProperty(propCode)
+			if !ok {
+				t.Errorf(errorMandatoryPropertyNotFound, propCode)
+			}
+			if len(prop.Data()) == 0 {
+				t.Errorf(errorMandatoryPropertyDataNotFound, propCode)
 			}
 		})
 	}
