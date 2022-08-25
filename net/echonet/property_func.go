eda82f63976a6ae855730fc169dcb503695bf0eb
Satoshi Konno <skonno@cybergarage.org>
2022-08-25 22:16:09 +0900

    Update propertyMapCodeToFormat2()

--- net/echonet/property_func.go
+++ net/echonet/property_func.go
@@ -4,15 +4,41 @@
 
 package echonet
 
-func propCodeToFormat2(propCode PropertyCode) (int, int, bool) {
+func propertyMapCodeToFormat2(propCode PropertyCode) (int, int, bool) {
 	if (propCode < PropertyCodeMin) || (PropertyCodeMax < propCode) {
 		return 0, 0, false
 	}
+	// 1 <= propCodeIdx <= 16
 	propCodeIdx := int(((propCode - PropertyCodeMin) & 0x0F)) + 1
+	// 0 <= propCodeIdx <= 7
 	propCodeBit := (((int(propCode-PropertyCodeMin) & 0xF0) >> 4) & 0x0F)
 	return propCodeIdx, propCodeBit, true
 }
 
-func isPropertyMapDescriptionFormat2(n int) bool {
+func isPropertyMapDescriptionFormat1(n int) bool {
 	return (n <= PropertyMapFormat1MaxSize)
+}
+
+func isPropertyMapDescriptionFormat2(n int) bool {
+	return !isPropertyMapDescriptionFormat1(n)
+}
+
+func propertyMapFormat2BitToCode(row int, bit int) PropertyCode {
+	// 0 <= bit <= 7
+	code := (0x10 * bit) + PropertyCodeMin
+	// 0 <= row <= 15
+	code += row
+	return PropertyCode(code)
+}
+
+func propertyMapFormat2ByteToCodes(row int, b byte) []PropertyCode {
+	codes := make([]PropertyCode, 0)
+	for n := 0; n < 8; n++ {
+		bit := byte((0x01 << n) & 0x0F)
+		if (b & bit) == 0 {
+			continue
+		}
+		codes = append(codes, propertyMapFormat2BitToCode(row, n))
+	}
+	return codes
 }
