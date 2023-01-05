04dca18bdda221786fd5c24be68b364d3dfd0452
Satoshi Konno <skonno@cybergarage.org>
2023-01-06 01:24:40 +0900

    Add uechobench

---
+++ examples/uechobench/search_controller.go
@@ -0,0 +1,28 @@
+// Copyright (C) 2018 The uecho-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"github.com/cybergarage/uecho-go/net/echonet"
+	"github.com/cybergarage/uecho-go/net/echonet/protocol"
+)
+
+type SearchController struct {
+	*echonet.Controller
+}
+
+func NewSearchController() *SearchController {
+	c := &SearchController{
+		Controller: echonet.NewController(),
+	}
+	return c
+}
+
+func (ctrl *SearchController) ControllerMessageReceived(msg *protocol.Message) {
+	// log.Infof("%s : %s\n", msg.From.String(), hex.EncodeToString(msg.Bytes()))
+}
+
+func (ctrl *SearchController) ControllerNewNodeFound(*echonet.RemoteNode) {
+}
