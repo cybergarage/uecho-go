bd0c8a5932e923735220b479d92e30944b7e06c8
dgfiloso <dgfiloso95@gmail.com>
2023-07-05 04:14:16 +0900

    Separate again some files between windows and linux because errors were found while testing with windows.

---
+++ net/echonet/transport/socket_reuseaddr_windows.go
@@ -0,0 +1,28 @@
+// Copyright 2018 The uecho-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build windows
+// +build windows
+
+package transport
+
+import (
+	"syscall"
+)
+
+// SetReuseAddr sets a flag to SO_REUSEADDR and SO_REUSEPORT.
+// nolint: nosnakecase
+func (sock *Socket) SetReuseAddr(fd uintptr, flag bool) error {
+	opt := 0
+	if flag {
+		opt = 1
+	}
+
+	err := syscall.SetsockoptInt(syscall.Handle(fd), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, opt)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
