1d0c1906a3633ef67292806e30dd9da5f40bf482
Satoshi Konno <skonno@cybergarage.org>
2023-07-06 02:20:05 +0900

    Update go:build flags for macOS platforms

--- net/echonet/transport/socket_reuseaddrport.go
+++ net/echonet/transport/socket_reuseaddrport.go
@@ -2,21 +2,19 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build bsd || freebsd || darwin
+//go:build (bsd || freebsd || darwin) && !windows
 // +build bsd freebsd darwin
+// +build !windows
 
 package transport
 
 import (
-	"os"
 	"syscall"
 )
 
 // SetReuseAddr sets a flag to SO_REUSEADDR and SO_REUSEPORT.
 // nolint: nosnakecase
-func (sock *Socket) SetReuseAddr(file *os.File, flag bool) error {
-	fd := file.Fd()
-
+func (sock *Socket) SetReuseAddr(fd uintptr, flag bool) error {
 	opt := 0
 	if flag {
 		opt = 1
