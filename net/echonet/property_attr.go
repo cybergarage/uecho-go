0d2edfbecc8a7670ee8f112349c4e82b4bd00509
Satoshi Konno <skonno@cybergarage.org>
2025-10-05 23:51:29 +0900

    refactor: rename PropertyAttr to PropertyAttribute

---
+++ net/echonet/property_attr.go
@@ -0,0 +1,29 @@
+// Copyright (C) 2018 The uecho-go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package echonet
+
+// PropertyAttribute is a type for property attribute.
+type PropertyAttribute uint
+
+const (
+	Prohibited = PropertyAttribute(0x00)
+	Required   = PropertyAttribute(0x01)
+	Optional   = PropertyAttribute(0x02)
+)
+
+// IsRequired returns true when the attribute is Required.
+func (attr PropertyAttribute) IsRequired() bool {
+	return attr == Required
+}
+
+// IsOptional returns true when the attribute is Optional.
+func (attr PropertyAttribute) IsOptional() bool {
+	return attr == Optional
+}
+
+// IsPermitted returns true when the attribute is not Prohibited.
+func (attr PropertyAttribute) IsProhibited() bool {
+	return attr == Prohibited
+}
