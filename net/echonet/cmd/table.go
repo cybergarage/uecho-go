37851c6b23adef8cb84254bb536558ced617b2ce
Satoshi Konno <skonno@cybergarage.org>
2025-10-12 21:51:01 +0900

    chaore: remove deprecate cmd.Table.HideDuplicateColumns()

--- net/echonet/cmd/table.go
+++ net/echonet/cmd/table.go
@@ -8,11 +8,6 @@ package cmd
 type Table interface {
 	Columns() []string
 	Rows() [][]string
-	TableHelper
-}
-
-type TableHelper interface {
-	HideDuplicateColumns(indexes ...int) Table
 }
 
 type table struct {
@@ -36,40 +31,4 @@ func (t *table) Columns() []string {
 // Rows returns the rows of the table.
 func (t *table) Rows() [][]string {
 	return t.rows
-}
-
-func (t *table) HideDuplicateColumns(columnIdxes ...int) Table {
-	stripDuplicateRowColumns := func(rows [][]string, columnIdx int) [][]string {
-		uniqRows := [][]string{}
-		lastRowContext := ""
-		for rowIdx, row := range rows {
-			uniqRow := make([]string, len(row))
-			copy(uniqRow, row)
-			switch {
-			case rowIdx == 0:
-				lastRowContext = row[columnIdx]
-			default:
-				if row[columnIdx] == lastRowContext {
-					switch columnIdx {
-					case 0:
-						uniqRow[columnIdx] = ""
-					default:
-						if uniqRow[columnIdx-1] == "" {
-							uniqRow[columnIdx] = ""
-						}
-					}
-				} else {
-					lastRowContext = row[columnIdx]
-				}
-			}
-			uniqRows = append(uniqRows, uniqRow)
-		}
-		return uniqRows
-	}
-
-	uniqRows := t.rows
-	for _, columnIdx := range columnIdxes {
-		uniqRows = stripDuplicateRowColumns(uniqRows, columnIdx)
-	}
-	return NewTable(t.columns, uniqRows)
 }
